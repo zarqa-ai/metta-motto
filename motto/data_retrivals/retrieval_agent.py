fe5e3f928904cb287013fdff42628ae640a83b89
sveta <bessveta>
2023-12-08 00:08:26 +0300

    changes

--- motto/data_retrivals/retrieval_agent.py
+++ motto/data_retrivals/retrieval_agent.py
@@ -1,7 +1,13 @@
+import chromadb
 from embedings_getters import OpenAIEmbeddings
 import os
+import pathlib
 import concurrent.futures
-class RetrivalAgent:
+
+from examples.motto.data_retrivals.doc_processor import DocProcessor
+
+
+class RetrievalAgent:
     def __init__(self, files_folder):
         if not os.path.exists(files_folder):
             return
@@ -9,10 +15,10 @@ class RetrivalAgent:
         self.__original_docs_folder = files_folder
         # we will store database in folder with name "data" in root directory of lib
         parent_dir = pathlib.Path(__file__).parent.resolve().parent
-        data_dir =  os.path.join(parent_dir, "data")
+        data_dir = os.path.join(parent_dir, "data")
         # the database is named as files directory
-        files_folder = os.os.path.dirname(files_folder)
-        db = os.path.join(data_dir,f"{files_folder}_embedings" )
+        files_folder = os.path.dirname(files_folder)
+        db = os.path.join(data_dir, f"{files_folder}_embedings")
         self.collection_name = f"{files_folder}_collection"
         need_load_docs = not os.path.exists(db)
         self.chroma_client = chromadb.PersistentClient(db)
@@ -22,13 +28,14 @@ class RetrivalAgent:
         if not need_load_docs:
             if self.collection.count() == 0:
                 need_load_docs = True
-                #shutil.rmtree(db)
+                # shutil.rmtree(db)
 
         if need_load_docs:
             self.__load_docs()
 
     def __process_doc(self, file):
         chunks = {}
+        text = DocProcessor.clear_text(file)
         chunks['texts'] = DocProcessor.get_text_chunks(text)
         chunks['embeddings'] = self.embeddings_getter.get_chunks_embeddings(chunks['texts'])
         length = len(chunks['texts'])
@@ -39,16 +46,18 @@ class RetrivalAgent:
     def __load_docs(self):
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = []
-            for file in os.path.listdir(self.__original_docs_folder):
+            for file in os.listdir(self.__original_docs_folder):
                 futures.append(
-                    executor.submit(self.process_doc,file=file )
+                    executor.submit(self.__process_doc, file=file)
                 )
             i = 0
             for future in concurrent.futures.as_completed(futures):
                 chunks = future.result()
+                length = len(chunks['texts'])
                 chunks['ids'] = [f"id{i + j}" for j in range(length)]
                 self.collection.add(embeddings=chunks['embeddings'], documents=chunks['texts'],
                                     metadatas=chunks['source'], ids=chunks['ids'])
-if __name__ == '__main__':
-    agent =  RetrivalAgent("/media/sveta/hdisk4/singnet/texts")
 
+
+if __name__ == '__main__':
+    agent = RetrievalAgent("/media/sveta/hdisk4/singnet/texts")
