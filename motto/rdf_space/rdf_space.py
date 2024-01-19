2a65aae30b41c780dcf9e7439206519c2eb1bd18
sveta <bessveta>
2024-01-19 22:16:54 +0300

    complex filters

--- motto/rdf_space/rdf_space.py
+++ motto/rdf_space/rdf_space.py
@@ -38,20 +38,21 @@ class QueryItems:
             if len(item) > 2 and item[0] == '"' and item[-1] == '"':
                 item = item[1:-1]
             if len(item) > 1 and item[0] == "$":
-                item = item[1:]
-                if item not in variables:
-                    variables.add(item)
-                item = f"?{item}"
+                item_name = item[1:]
+                if item_name not in variables:
+                    variables.add(item_name)
 
             str_items.append(item)
 
         return cls(str_items, variables)
+
 
 class SPARQLComponents:
     def __init__(self):
         self.conditions = []
         self.filters = []
         self.filter_not_exists = []
+        self.filter_exists = []
         self.limit = ""
         self.variables = set()
         self.distinct = False
@@ -80,8 +81,32 @@ class ServiceFeatures:
             self.service = "<https://dbpedia.org/sparql>"
 
 
-
 class RdfHelper:
+    complex_filters = [ "filter_exists", "filter_not_exists", "union"]
+
+    @staticmethod
+    def is_simple_condition(atom):
+        if hasattr(atom, 'get_children'):
+            children = atom.get_children()
+            if len(children) > 0:
+                if hasattr(children[0], 'get_children'):
+                    return False
+        return True
+
+    @staticmethod
+    def collect_components(atom):
+        if not RdfHelper.is_simple_condition(atom):
+            children = atom.get_children()
+        else:
+            children = [atom]
+        components = []
+        variables = set()
+        for child in children:
+            s = RdfHelper.get_fields_and_conditions(E(child)) \
+                if RdfHelper.is_simple_condition(child) else RdfHelper.get_fields_and_conditions(child)
+            components.append(s)
+            variables = variables.union(s.variables)
+        return components, variables
 
     @staticmethod
     def get_fields_and_conditions(query_atom):
@@ -100,11 +125,20 @@ class RdfHelper:
                 elif len(items) == 2:
                     first = repr(items[0]).lower()
                     query_item = QueryItems.from_atoms(items[1:])
-                    if first == "filter":
+                    # filters can contain union, union can contain filter, for these components we create separate
+                    # SPARQLComponent
+                    if (first in RdfHelper.complex_filters) and (hasattr(items[1], 'get_children')):
+                        components, variables = RdfHelper.collect_components(items[1])
+                        if first == "filter_exists":
+                            query_components.filter_exists.append(components)
+                            query_components.variables = query_components.variables.union(variables)
+                        elif first == "filter_not_exists":
+                            query_components.filter_not_exists.append(components)
+                        elif first == "union":
+                            query_components.union.append(components)
+                            query_components.variables = query_components.variables.union(variables)
+                    elif first == "filter":
                         query_components.filters.append(" ".join(query_item.str_items))
-                        query_components.variables = query_components.variables.union(query_item.variables)
-                    elif first == "filter_not_exists":
-                        query_components.filter_not_exists.append(" ".join(query_item.str_items))
                         query_components.variables = query_components.variables.union(query_item.variables)
                     elif first == "limit":
                         query_components.limit = repr(items[1])
@@ -117,18 +151,13 @@ class RdfHelper:
                     elif first == "order_desc":
                         query_components.order_desc.extend(query_item.str_items)
                     elif first == "optional":
-                        s = RdfHelper.get_fields_and_conditions(E(items[1]))
+                        # optional can have filters and conditions, so for 'optional' we create SPARQLComponent
+                        s = RdfHelper.get_fields_and_conditions(E(items[1])) \
+                            if RdfHelper.is_simple_condition(items[1]) else RdfHelper.get_fields_and_conditions(
+                            items[1])
                         query_components.optional.append(s)
                         query_components.variables = query_components.variables.union(s.variables)
-                    elif first == "union":
-                        if hasattr(items[1], 'get_children'):
-                            children = items[1].get_children()
-                            components = []
-                            for child in children:
-                                s = RdfHelper.get_fields_and_conditions(E(child))
-                                components.append(s)
-                                query_components.variables = query_components.variables.union(s.variables)
-                            query_components.union.append(components)
+
                     else:
                         raise ValueError(f"Unexpected query item")
         return query_components
@@ -143,18 +172,39 @@ class RdfSpace(GroundingSpace):
     def from_space(self, cspace):
         self.gspace = GroundingSpaceRef(cspace)
 
-    def __collect_conditions(self, query_components):
+    def __collect_inner_conditions(self, components, add_dots=True):
+        conditions = []
+        for component in components:
+            condition = self.__collect_conditions(component, add_dots)
+            conditions.append(condition)
+        return conditions
+
+    def __collect_conditions(self, query_components, add_dots=True):
         sparql_query = ""
         for condition in query_components.conditions:
-            sparql_query += condition + " . \n"
+            sparql_query += condition + " . \n" if add_dots else condition
         for filter in query_components.filters:
             sparql_query += f"filter ({filter})\n"
 
         if len(query_components.filter_not_exists) > 0:
             sparql_query += "filter not exists {"
             for filter in query_components.filter_not_exists:
-                sparql_query += f"{filter} .\n"
+                conditions = self.__collect_inner_conditions(filter)
+                sparql_query += " ".join(conditions)
             sparql_query += "}"
+
+        if len(query_components.filter_exists) > 0:
+            sparql_query += "filter exists {"
+            for filter in query_components.filter_exists:
+                conditions = self.__collect_inner_conditions(filter)
+                sparql_query += " ".join(conditions)
+            sparql_query += "}"
+
+        if len(query_components.union) > 0:
+            for union in query_components.union:
+                conditions = self.__collect_inner_conditions(union)
+                if len(conditions) > 0:
+                    sparql_query += "{{" + "} UNION {".join(conditions) + "}}\n"
         return sparql_query
 
     def construct_query(self, query_atom):
@@ -162,7 +212,7 @@ class RdfSpace(GroundingSpace):
         sparql_query = ""
         for key, prefix in self.service_features.prefixes.items():
             sparql_query += f"PREFIX {key}: {prefix}" + "\n"
-        fixed_variables = " ".join([f"?{var}" for var in query_components.variables])
+        fixed_variables = " ".join([f"${var}" for var in query_components.variables])
 
         sparql_query += f"SELECT distinct {fixed_variables}\n" if query_components.distinct else f"SELECT {fixed_variables}\n"
         if self.service_features.service or len(query_components.conditions) > 0 or len(query_components.filters) > 0:
@@ -171,15 +221,6 @@ class RdfSpace(GroundingSpace):
                 sparql_query += f"SERVICE {self.service_features.service}" + "{\n"
 
             sparql_query += self.__collect_conditions(query_components)
-
-            if len(query_components.union) > 0:
-                for union in query_components.union:
-                    union_conditions = []
-                    for component in union:
-                        condition = self.__collect_conditions(component)
-                        union_conditions.append(f"{{{condition}}}")
-                    if len(union_conditions) > 0:
-                        sparql_query += "{" + " UNION ".join(union_conditions) + "}\n"
 
             if len(query_components.optional) > 0:
                 optional_conditions = ""
@@ -203,7 +244,6 @@ class RdfSpace(GroundingSpace):
             sparql_query += f"Limit {query_components.limit} "
         if query_components.offset:
             sparql_query += f"OFFSET {query_components.offset} "
-
 
         return sparql_query, query_components.variables
 
