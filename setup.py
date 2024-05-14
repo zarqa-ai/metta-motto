22e948d7d4b5b1ff369c660b56efbf41f0411860
Maxim Peterson <maxim.peterson@gmail.com>
2024-05-14 18:25:59 +0300

    pypi test

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+       name="test-metta-motto",
+       version="0.0.1",  # Update as appropriate
+       description="Integration of MeTTa and LLMs for prompt templates, guidance, and chaining as well as composition with other agents.",
+       long_description=long_description,
+       long_description_content_type="text/markdown",
+       url="https://github.com/zarqa/metta-motto",
+       packages=find_packages(),
+       python_requires='>=3.8',
+       install_requires=[
+        # List your project's dependencies here.
+        'hyperon'
+        ]
+   )
