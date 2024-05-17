00b224d24358f28cf5337be8147815fed94314f1
sveta <bessveta>
2024-05-17 18:01:35 +0300

    fix

--- motto/langchain_agents/langchain_agent.py
+++ motto/langchain_agents/langchain_agent.py
@@ -1,6 +1,11 @@
 from motto.agents import DialogAgent
-from hyperon import ExpressionAtom, OperationAtom, ValueAtom, E, S
+from hyperon import ExpressionAtom, OperationAtom, ValueAtom, E, S, G, AtomType, OperationObject, ValueObject
 from hyperon.ext import register_atoms
+from hyperon.stdlib import get_py_atom
+
+default_model = E(E(OperationAtom("py-atom", get_py_atom, unwrap=False), S("langchain_openai.ChatOpenAI")),
+                  E(S("Kwargs"), E(S("model"), G(ValueObject("gpt-3.5-turbo-0125"))),
+                    E(S("temperature"), G(ValueObject(0)))))
 
 
 class LangchainAgent(DialogAgent):
@@ -12,14 +17,11 @@ class LangchainAgent(DialogAgent):
 
     def _prepare(self, metta, msgs_atom):
         super()._prepare(metta, msgs_atom)
-
-        metta.space().add_atom(E(S('='), E(S('history')),
-                                 E(*self.history)))
+        res = metta.run(repr(self.model))
+        if len(res) == 0:
+            self.model = default_model
         metta.space().add_atom(E(S('='), E(S('langchain-model')),
                                  self.model))
-        # atm, we put the input message into the history by default
-
-        self.history += [msgs_atom]
 
 
 @register_atoms(pass_metta=True)
