62fbc71e3faf6a488e5f1ee22177271f8c653053
sveta <bessveta>
2024-05-30 10:04:30 +0300

    support multiple messages for langchain agent

--- motto/utils.py
+++ motto/utils.py
@@ -1,8 +1,11 @@
 import os
 import sys
+from hyperon import *
+
+
 def get_string_value(atom) -> str:
     item = repr(atom)
-    if len(item) > 2 and (item[0] == '"' and item[-1] == '"') :
+    if len(item) > 2 and (item[0] == '"' and item[-1] == '"'):
         item = item[1:-1]
     return item
 
@@ -18,5 +21,22 @@ def concat_str(left, right) -> str:
     str2 = get_string_value(right)
     return str1 + str2
 
-# convert nested tuples to nested python tuples or lists
 
+def process_inner(inner_children):
+    if len(inner_children) == 2:
+        return (get_string_value(inner_children[0]), get_string_value(inner_children[1]))
+    raise ValueError()
+
+
+def message2tuple(msg_atom):
+    messages = []
+    if hasattr(msg_atom, 'get_children'):
+        children = msg_atom.get_children()
+        if (len(children) > 1) and repr(children[0]) == "Messages":
+            for ch in children[1:]:
+                if hasattr(ch, 'get_children'):
+                    inner_children = ch.get_children()
+                    messages.append(process_inner(inner_children))
+        else:
+            messages.append(process_inner(children))
+    return messages
