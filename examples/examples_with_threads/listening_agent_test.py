4c4224f4930e8318a89aea591c4246e023df3054
Alexey Potapov <pas.aicv@gmail.com>
2024-11-21 19:58:14 +0300

    whitespace

--- examples/examples_with_threads/listening_agent_test.py
+++ examples/examples_with_threads/listening_agent_test.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
       ! (bind! &a1 (listening-agent "simple_call.msa"))
       ! (&a1)
       ! (println! "Agent is running") 
-      ! (&a1 .input (py-dict (("message" "who is the 6 president of France")("additional_info" (py-list ((py-tuple ("model_name" "gpt-3.5-turbo" "String"))))))))
+      ! (&a1 .input (py-dict (("message" "who is the 6 president of France") ("additional_info" (py-list ((py-tuple ("model_name" "gpt-3.5-turbo" "String"))))))))
       ! ((py-atom time.sleep) 3)
       ! (&a1 .get_output)
       ! (&a1 .input "who is John Lennon?")
